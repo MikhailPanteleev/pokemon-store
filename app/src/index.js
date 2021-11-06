c1f79a70a01649f481e60960cd6cf3ba28425cc3
MikhailPanteleev <l.slaanesh@mail.ru>
2021-11-07 00:18:52 +0300

    first commit

---
+++ app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Pokemon Store!!!!!!!!!!!!</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
